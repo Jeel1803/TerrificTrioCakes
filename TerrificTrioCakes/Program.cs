26660eab4a9dc0aa1b6478c40cd22b42c5b8612b
Akhil Eldhose <eldh4@manukaumail.com>
2022-11-14 10:09:13 +1300

    Server Connected

--- TerrificTrioCakes/Program.cs
+++ TerrificTrioCakes/Program.cs
@@ -10,6 +10,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+
 builder.Services.AddDbContext<CakeShopContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();

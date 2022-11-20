acc3e6075110c434264291c237e164a47d7a5e45
Akhil Eldhose <eldh4@manukaumail.com>
2022-11-20 13:05:34 +1300

    Contact Us Form

--- TerrificTrioCakes/wwwroot/js/site.js
+++ TerrificTrioCakes/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//Contact Us
+function contactUs() {
+    let name = document.forms["contact"]["name"].value;
+    let email = document.forms["contact"]["email"].value;
+    let message = document.forms["contact"]["phone"].value;
+    let message = document.forms["contact"]["message"].value;
+    let subject = document.forms["contact"]["subject"].value;
 
-// Write your JavaScript code.
+    if (name == "" && email == "" && phone=="" && message == "" && subject == "") {
+        alert("No information Entered")
+    }
+    else {
+        alert("Querey Submitted")
+    }
+}
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click', () => {
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () => {
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () => {
+    loginForm.classList.remove('active');
+});
